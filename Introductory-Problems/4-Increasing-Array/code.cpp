f48d44c6691b4d474f476b8748efa49f9c80eed9
Nabin Sharma <sharmanabin.0910@gmail.com>
2024-07-08 18:23:46 +0530

    Increasing Array

---
+++ Introductory-Problems/4-Increasing-Array/code.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+#define ll long long
+#define vl(x,n); vector<long long> x(n,0);
+#define take(v); for(auto &i:v) cin>>i;
+int main()
+{
+    ll n;
+    cin>>n;
+    vl(a,n); take(a);
+    ll ans=0;
+    for(ll i = 1; i<n; i++){
+        if(a[i]>=a[i-1]){
+            continue;
+        }
+        else{
+            ans = ans + a[i-1] - a[i]; 
+            a[i] = a[i-1];
+        }
+    }
+    cout<<ans;
+    return 0;
+}
